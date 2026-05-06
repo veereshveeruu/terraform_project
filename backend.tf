4d1e3056564bba7a95ed2c7e31df689745a7f1e4
veeresh <veereshvirat2134@gmail.com>
2026-05-06 13:10:09 +0530

    changed bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "myterraform-bucket"
+    bucket         = "myterraform-bucket-usw2"
     key            = "terraform/state.tfstate"
     region         = "us-west-2"
     encrypt        = true
